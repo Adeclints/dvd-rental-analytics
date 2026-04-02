65dff173f8b3e016da2fac984e499bd895c4cea4
Adeclints <clintonbilley@gmail.com>
2026-04-02 14:58:44 +0300

    Add complete project structure

--- sql/03_business_questions.sql
+++ sql/03_business_questions.sql
@@ -95,3 +95,62 @@ ORDER BY total_rentals ASC, inventory_copies DESC, f.title
 LIMIT 15;
 
 
+-- =========================================================
+-- 6. Which films were never rented?
+-- =========================================================
+SELECT
+    f.film_id,
+    f.title,
+    COUNT(DISTINCT i.inventory_id) AS inventory_copies,
+    COUNT(r.rental_id) AS total_rentals
+FROM film f
+JOIN inventory i
+    ON f.film_id = i.film_id
+LEFT JOIN rental r
+    ON i.inventory_id = r.inventory_id
+GROUP BY f.film_id, f.title
+HAVING  COUNT(r.rental_id) = 0
+ORDER BY total_rentals ASC, inventory_copies DESC, f.title
+LIMIT 15;
+
+-- =========================================================
+-- 7. Which customers spend the most?
+-- =========================================================
+
+select c.first_name, c.last_name, round(SUM(p.amount ),2) as Expenditure
+from customer c 
+join payment p 
+on p.customer_id =c.customer_id 
+group by c.first_name, c.last_name
+order by Expenditure desc
+limit 10;
+
+-- =========================================================
+-- 8. Which customers rent most frequently?
+-- =========================================================
+
+select 
+c.first_name, 
+c.last_name, 
+round(count(r.rental_id  ),2) as Total_Rentals
+from customer c 
+join rental r  on r.customer_id = c.customer_id
+group by c.first_name, c.last_name
+order by Total_Rentals desc
+limit 10;
+
+-- =========================================================
+-- 9. Which store performs better by rentals and revenue?
+-- =========================================================
+
+select s.store_id, 
+Count(r.rental_id) as Total_Rentals,
+round(sum(p.amount),1) as Revenue
+from store s 
+join inventory i on i.store_id = s.store_id 
+join rental r  on r.inventory_id =i.inventory_id 
+join payment p on p.rental_id =r.rental_id 
+group by s.store_id
+order by  Total_Rentals desc, Revenue desc;
+
+
